8873cca553a107c172a2ef73bb76e7ffd1a9f528
James Mann <james.mann@bcs.org>
2017-01-30 21:59:52 +0000

    Add project files.

---
+++ Bot Application3/Controllers/CarInsuranceDialog.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Threading.Tasks;
+
+namespace Bot_Application3.Controllers
+{
+[Serializable]
+public class CarInsuranceDialog : IDialog<object>
+{
+    protected string registrationNumber { get; set; }
+    protected string startDate { get; set; }
+
+    public async Task StartAsync(IDialogContext context)
+    {
+        context.Wait(MessageReceivedStartConversation); // State transition: wait for user to start conversation
+    }
+
+    public async Task MessageReceivedStartConversation(IDialogContext context, IAwaitable<IMessageActivity> argument)
+    {
+        await context.PostAsync("What's your registration number?");
+        context.Wait(MessageReceivedRegistrationNumber); // State transition: wait for user to provide registration number
+    }
+
+    public async Task MessageReceivedRegistrationNumber(IDialogContext context, IAwaitable<IMessageActivity> argument)
+    {
+        this.registrationNumber = (await argument).Text;
+        await context.PostAsync("When do you want cover to start?");
+        context.Wait(MessageReceivedCoverStart); // State transition: wait for user to provide cover start date
+    }
+
+    public async Task MessageReceivedCoverStart(IDialogContext context, IAwaitable<IMessageActivity> argument)
+    {
+        this.startDate = (await argument).Text;
+        // do your search/aggregation here
+        await context.PostAsync($"OK, I found these deals for {registrationNumber} starting {startDate}...");
+        context.Done<object>(new object()); // Signal completion
+    }
+}
+}
---
+++ Bot Application3/Controllers/MessagesController.cs
@@ -0,0 +1,24 @@
+﻿using Bot_Application3.Controllers;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace dialogs_basic
+{
+[BotAuthentication]
+public class MessagesController : ApiController
+{
+    public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+    {
+        if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+        {
+            // Transition into the CarInsuranceDialog
+            await Conversation.SendAsync(activity, () => new CarInsuranceDialog());
+        }
+
+        return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+    }
+}
+}
